aacd41a94e2f2b975eacc7044b0e753fba46b104
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-07-01 20:39:16 +0500

    Create Trivia-Bot.js

---
+++ Projects/Trivia-Bot.js
@@ -0,0 +1,31 @@
+
+// 1. Log the greeting message
+console.log("Hello! I'm your coding fun fact guide!");
+
+// 2-7. Declare variables for the bot's details
+let botName = "FactBot";
+let botLocation = "the Cloud";
+let favoriteLanguage = "JavaScript";
+
+// 8. Log the introduction sentence using variables
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+// 9. Log the favorite language sentence using the variable
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// 10-12. Use 'let' to declare codingFact with favoriteLanguage, then log it
+let codingFact = favoriteLanguage + " was famously created in just 10 days back in 1995!";
+console.log(codingFact);
+
+// 13. Reassign codingFact with a second fact using favoriteLanguage, then log it
+codingFact = "Even though it has 'Java' in the name, " + favoriteLanguage + " is completely unrelated to the Java programming language.";
+console.log(codingFact);
+
+// 14. Reassign codingFact with a third fact using favoriteLanguage, then log it
+codingFact = "Over 98% of all websites use " + favoriteLanguage + " for their front-end client-side behavior.";
+console.log(codingFact);
+
+// 15. Log the final farewell statement using variables
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
