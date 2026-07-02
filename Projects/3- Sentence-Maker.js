ade490d2e40cef5d2307c1572c13da146d7ff2ab
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-07-02 20:18:55 +0500

    Create 3- Sentence-Maker.js

---
+++ Projects/3- Sentence-Maker.js
@@ -0,0 +1,40 @@
+
+
+// 1-6. Declare the initial variables using let
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+// 7-12. Assign initial string values of your choice
+adjective = "friendly";
+noun = "dragon";
+verb = "excited";
+place = "castle";
+adjective2 = "tiny";
+noun2 = "tacos";
+
+// 13-15. Declare firstStory and assemble it using the template precisely
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 16. Log the first story matching the "First story: [firstStory]" requirement
+console.log("First story: " + firstStory);
+
+// 18-23. Reassign new values to the variables for the second story
+adjective = "grumpy";
+noun = "monster";
+verb = "scared";
+place = "cave";
+adjective2 = "giant";
+noun2 = "pancakes";
+
+// 17, 24-25. Declare secondStory and assemble it using the exact same template format
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 26. Log the second story matching the "Second story: [secondStory]" requirement
+console.log("Second story: " + secondStory);
+
+
+
