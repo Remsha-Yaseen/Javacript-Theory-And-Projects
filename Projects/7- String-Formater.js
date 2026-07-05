f7e916faf260f94b310b8099d86c9eb5739105e1
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-07-05 14:00:07 +0500

    Create 7- String-Formater.js

---
+++ Projects/7- String-Formater.js
@@ -0,0 +1,7 @@
+
+// ... upper lines of code remain unchanged ...
+let lowercaseWord = "camelcase";
+let camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3);
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
+
