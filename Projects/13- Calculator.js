faf6744ea8f903990a173fd102116eacd2e31f08
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-07-09 21:03:15 +0500

    Create 13- Calculator.js

---
+++ Projects/13- Calculator.js
@@ -0,0 +1,16 @@
+
+
+
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
+
+console.log(calculateSquareRoot(25));
+console.log(calculateSquareRoot(100));
+
+
+------boolean-check-function-------
+
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
