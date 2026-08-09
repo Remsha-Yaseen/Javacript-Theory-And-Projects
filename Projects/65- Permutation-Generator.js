184cc95a6c8a498d9eb149a495472b6340a6b849
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-08-09 08:52:47 +0500

    Create  65- Permutation-Generator.js

---
+++ Projects/65- Permutation-Generator.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+function permuteString(str, prefix = "", results = []) {
+  if (str.length === 0) {
+    if (!results.includes(prefix)) {
+      results.push(prefix);
+    }
+    return results;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const remaining = str.slice(0, i) + str.slice(i + 1);
+    permuteString(remaining, prefix + str[i], results);
+  }
+
+  return results;
+}
+
+
+
