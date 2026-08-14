1846eac295ca4158247d64fdab8000078a15421a
Remsha Yaseen <256290848+Remsha-Yaseen@users.noreply.github.com>
2026-08-14 12:36:23 +0500

    Create  69- Implement-Queue.js

---
+++ Projects/69- Implement-Queue.js
@@ -0,0 +1,38 @@
+
+
+
+function initQueue() {
+  return {
+    collection: []
+  };
+}
+
+function print(queue) {
+  console.log(queue.collection);
+}
+
+function enqueue(queue, element) {
+  queue.collection.push(element);
+}
+
+function dequeue(queue) {
+  return queue.collection.shift();
+}
+
+function front(queue) {
+  if (queue.collection.length === 0) {
+    return undefined;
+  }
+  return queue.collection[0];
+}
+
+function size(queue) {
+  return queue.collection.length;
+}
+
+function isEmpty(queue) {
+  return queue.collection.length === 0;
+}
+
+
+
